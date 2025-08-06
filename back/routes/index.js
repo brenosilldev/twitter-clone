a7d8a9c42bbf3919ca55314cddbf9014714a3f4b
Breno Silva <brenosill@hotmail.com>
2025-08-06 10:38:58 -0300

    feat: Incluido nova rota

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,12 +2,14 @@ import {Router} from "express";
 import AuthRouter from "./auth.js";
 import UserRouter from "./user.js";
 import PostRouter from "./post.js";
+import NotificationRouter from "./notification.js";
 
 const Routes = Router();
 
 Routes.use('/auth',AuthRouter);
 Routes.use('/user',UserRouter);
 Routes.use('/post',PostRouter);
+Routes.use('/notification',NotificationRouter);
 
 export default Routes
 
